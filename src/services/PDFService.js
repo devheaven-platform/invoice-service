0123c9ff7296f25720f2b2a73540133beb761d3c
Mario <mario.olislaegers@gmail.com>
2019-05-14 11:03:15 +0200

    PDFService

--- src/services/PDFService.js
+++ src/services/PDFService.js
@@ -2,15 +2,17 @@ const path = require( "path" );
 const fs = require( "fs" );
 const puppeteer = require( "puppeteer" );
 
+generatePDF().then();
 async function generatePDF( data ) {
-    const content = fs.readFileSync( "apth", "utf8" );
+    const content = fs.readFileSync( "./invoice.html", "utf8" );
 
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.setContent( content );
-    await page.emulateMedia( "screen" );
+    await page.setViewport({ width: 1920, height: 1080});
+    await page.emulateMedia( "print" );
     await page.pdf( {
-        path: "filename",
+        path: "invoice.pdf",
         format: "A4",
         printBackground: true,
     } );
