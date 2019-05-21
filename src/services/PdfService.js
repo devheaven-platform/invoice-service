cccdaa79e36af6b318955a1987de3b1df2ffcba9
tomdewildt <t.dewildt@student.fontys.nl>
2019-05-21 10:01:25 +0200

    Add more params to pdf service

--- src/services/PdfService.js
+++ src/services/PdfService.js
@@ -10,15 +10,15 @@ const puppeteer = require( "puppeteer" );
 const generate = async ( invoice, project ) => {
     let content = fs.readFileSync( "src/assets/invoice.html", "utf8" );
 
-    const items = invoice.items.map( item => `<tr class="item"><td>${ item.description }</td><td>$${ item.cost.toFixed( 2 ) }</td></tr>` ).join( "" );
+    const items = invoice.items.map( item => `<tr class="item"><td>${ item.description }</td><td>$${ item.cost }</td></tr>` );
 
     content = content.replace( "{items}", items );
     content = content.replace( "{project-name}", project.name );
     content = content.replace( "{invoice-name}", invoice.name );
     content = content.replace( "{invoice-date}", new Date( invoice.createdAt ).toLocaleDateString() );
-    content = content.replace( "{total-cost}", invoice.total.toFixed( 2 ) );
+    content = content.replace( "{total-cost}", invoice.total );
 
-    const browser = await puppeteer.launch( { args: [ "--no-sandbox" ] } );
+    const browser = await puppeteer.launch( { headless: true, args: [ "--no-sandbox", "--disable-dev-shm-usage" ] } );
     const page = await browser.newPage();
     await page.setContent( content );
     await page.setViewport( { width: 1920, height: 1080 } );
