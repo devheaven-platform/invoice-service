40edc60fc8868e8e7f5da0857f5c6972492f382c
Liwus <liweihu12@gmail.com>
2019-05-28 16:47:20 +0200

    Remove stream.end

--- src/services/PdfService.js
+++ src/services/PdfService.js
@@ -19,8 +19,7 @@ const generate = async ( invoice, project ) => {
 
     pdf.create( content, { format: "a4" } ).toStream( ( err, stream ) => {
         if ( err ) return err;
-        const newStream = stream.pipe( fs.createWriteStream( `invoices/${ invoice.id }.pdf` ) );
-        newStream.end();
+        stream.pipe( fs.createWriteStream( `invoices/${ invoice.id }.pdf` ) );
     } );
 };
 
