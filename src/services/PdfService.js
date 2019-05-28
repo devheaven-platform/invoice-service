23bb2f994be3fd7be0b0b23e814b9c7f0c9bb03f
mario133710 <mario.olislaegers@gmail.com>
2019-05-28 12:12:22 +0200

    Puppeteer -> HTML-PDF (#27)
    * PDF
    
    * Deleted startup generate
    
    * Test new pdf shite

--- src/services/PdfService.js
+++ src/services/PdfService.js
@@ -1,34 +1,28 @@
-const fs = require( "fs" );
-const puppeteer = require( "puppeteer" );
 
+const fs = require("fs");
+const pdf = require("html-pdf");
 /**
  * Generates a pdf given a invoice and project.
  *
  * @param {Object} invoice the invoice object.
  * @param {Object} project the project object.
  */
-const generate = async ( invoice, project ) => {
-    let content = fs.readFileSync( "src/assets/invoice.html", "utf8" );
+const generate = async (invoice, project) => {
+    let content = fs.readFileSync("src/assets/invoice.html", "utf8");
 
-    const items = invoice.items.map( item => `<tr class="item"><td>${ item.description }</td><td>$${ item.cost.toFixed( 2 ) }</td></tr>` ).join( "" );
+    const items = invoice.items.map(item => `<tr class="item"><td>${item.description}</td><td>$${item.cost.toFixed(2)}</td></tr>`).join("");
 
-    content = content.replace( "{items}", items );
-    content = content.replace( "{project-name}", project.name );
-    content = content.replace( "{invoice-name}", invoice.name );
-    content = content.replace( "{invoice-date}", new Date( invoice.createdAt ).toLocaleDateString() );
-    content = content.replace( "{total-cost}", invoice.total.toFixed( 2 ) );
+    content = content.replace("{items}", items);
+    content = content.replace("{project-name}", project.name);
+    content = content.replace("{invoice-name}", invoice.name);
+    content = content.replace("{invoice-date}", new Date(invoice.createdAt).toLocaleDateString());
+    content = content.replace("{total-cost}", invoice.total.toFixed(2));
 
-    const browser = await puppeteer.launch( { devtools: false, headless: true, args: [ "--no-sandbox", "--disable-dev-shm-usage", "--disable-software-rasterizer", "--disable-gpu" ] } );
-    const page = await browser.newPage();
-    await page.setContent( content );
-    await page.setViewport( { width: 1920, height: 1080 } );
-    await page.emulateMedia( "print" );
-    await page.pdf( {
-        path: `invoices/${ invoice.id }.pdf`,
-        format: "A4",
-        printBackground: true,
-    } );
-    await browser.close();
+    var config = { format: 'A4' }; 
+    pdf.create(html, config).toFile(`invoices/${ invoice.id }.pdf`, function (err, res) {
+        if (err) return console.log(err);
+    });
+
 };
 
 module.exports = {
