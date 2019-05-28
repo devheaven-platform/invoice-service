b6b958198933f6a2b0b9753dee9b18d29f39eb47
Mario <mario.olislaegers@gmail.com>
2019-05-28 14:14:20 +0200

    HTML

--- src/services/PdfService.js
+++ src/services/PdfService.js
@@ -1,5 +1,5 @@
 const fs = require( "fs" );
-const wkhtmltopdf = require( "wkhtmltopdf" );
+
 /**
  * Generates a pdf given a invoice and project.
  *
@@ -7,7 +7,7 @@ const wkhtmltopdf = require( "wkhtmltopdf" );
  * @param {Object} project the project object.
  */
 const generate = async ( invoice, project ) => {
-    let content = fs.readFileSync( "src/assets/invoice.html", "utf8" );
+    let content = fs.readFileSync( "invoice.html", "utf8" );
 
     const items = invoice.items.map( item => `<tr class="item"><td>${ item.description }</td><td>$${ item.cost.toFixed( 2 ) }</td></tr>` ).join( "" );
 
@@ -16,8 +16,13 @@ const generate = async ( invoice, project ) => {
     content = content.replace( "{invoice-name}", invoice.name );
     content = content.replace( "{invoice-date}", new Date( invoice.createdAt ).toLocaleDateString() );
     content = content.replace( "{total-cost}", invoice.total.toFixed( 2 ) );
+    fs.writeFile( `/invoices/${ invoice.id }.html`, content, ( err ) => {
+        if ( err ) {
+            return console.log( err );
+        }
 
-    wkhtmltopdf( content, { output: `invoices/${ invoice.id }.pdf` } );
+        return console.log( "The file was saved!" );
+    } );
 };
 
 module.exports = {
